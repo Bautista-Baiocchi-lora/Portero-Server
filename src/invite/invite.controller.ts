f7dbeb2e013c34210ce1b2010e3752daf80577e6
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-04-15 11:36:17 -0300

    fixed device id session bug

--- src/invite/invite.controller.ts
+++ src/invite/invite.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import SessionGuard from 'src/session/session.guard';
 
 @Controller('invite')
 export default class InviteController {
   constructor() {}
+
+  @Post('claim')
+  @UseGuards(SessionGuard)
+  async claim(@Body() data: any) {}
 }
