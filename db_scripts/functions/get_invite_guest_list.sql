7a77162f3173e1bfda7bd689d86fe95600399788
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-07-05 21:06:22 -0300

    get guest list query now returns guest status

--- db_scripts/functions/get_invite_guest_list.sql
+++ db_scripts/functions/get_invite_guest_list.sql
@@ -1,12 +1,18 @@
 create or replace function get_invite_guest_list(
     invite_idf uuid
 )
-returns table(g_id uuid, g_doc text, g_fn text, g_ln text) as $$
+returns table(g_id uuid, g_doc text, g_fn text, g_ln text, g_entered timestamp without time zone, g_rejected timestamp without time zone, g_exited timestamp without time zone) as $$
     begin
         return query
-        select g.id, g.doc_id, g.first_name, g.last_name
-        from 
-        invite i right join guest g on i.id = g.invite_id
+        select g.id, g.doc_id, g.first_name, g.last_name, ge.entered, gr.rejected, gx.exited
+        from  
+        (guest g inner join invite i on g.invite_id = i.id)
+        left join 
+        guest_entered ge on ge.guest_id = g.id
+		left join
+		guest_rejected gr on gr.guest_id = g.id
+		left join
+		guest_exited gx on gx.guest_id = g.id
         where i.id = invite_idf;
     end
 $$ language plpgsql;
