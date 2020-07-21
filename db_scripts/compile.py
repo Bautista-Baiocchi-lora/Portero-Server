1529361771853a253f6ebe460b40f7899dacef10
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-07-21 14:21:44 -0300

    added select_propietarios_by_barrio to compile

--- db_scripts/compile.py
+++ db_scripts/compile.py
@@ -48,7 +48,8 @@ functions = [
      './functions/insert_person.sql',
      './functions/insert_guests_exited.sql',
      './functions/disable_propietario.sql',
-     './functions/disable_guardia.sql'
+     './functions/disable_guardia.sql',
+     './functions/select_propietarios_by_barrio.sql'
 ]
 
 scripts = types + tables + functions
@@ -81,7 +82,7 @@ with open('drop_db.sql', 'wt') as drop_db_file:
     
     #Drop functions
     drop_db_file.write("--Drop functions\n")
-    drop_db_file.write("drop function if exists disable_guardia, disable_propietario, can_guest_exit, insert_guests_exited, insert_person, is_propietario, select_invites_by_propietario, select_barrio_user, select_person_user, can_reject_guest, insert_guests_entered, insert_guests_rejected, can_guest_enter, get_invite_guest_list, select_invite_as_guardia, can_be_guardia, is_user, can_be_propietario, delete_lote, insert_person, insert_barrio_session, insert_user_session, insert_barrio, insert_guardia, insert_invite, insert_lote, insert_message, insert_propietario, insert_message, insert_session, insert_user, select_account, select_guardias_by_barrio, select_lotes_by_barrio, select_lotes_by_propietario, verify_session;\n")
+    drop_db_file.write("drop function if exists select_propietarios_by_barrio, disable_guardia, disable_propietario, can_guest_exit, insert_guests_exited, insert_person, is_propietario, select_invites_by_propietario, select_barrio_user, select_person_user, can_reject_guest, insert_guests_entered, insert_guests_rejected, can_guest_enter, get_invite_guest_list, select_invite_as_guardia, can_be_guardia, is_user, can_be_propietario, delete_lote, insert_person, insert_barrio_session, insert_user_session, insert_barrio, insert_guardia, insert_invite, insert_lote, insert_message, insert_propietario, insert_message, insert_session, insert_user, select_account, select_guardias_by_barrio, select_lotes_by_barrio, select_lotes_by_propietario, verify_session;\n")
 
     #Drop types
     drop_db_file.write("--Drop types\n")
--- db_scripts/functions/select_propietarios_by_barrio.sql
+++ db_scripts/functions/select_propietarios_by_barrio.sql
@@ -1,11 +1,13 @@
-create or replace function select_propietarios_by_barrio(barrio_idf uuid)
+create or replace function select_propietarios_by_barrio(
+    barrio_idf uuid
+)
 returns table(prop_id uuid, prop_since timestamp without time zone, email text, first_name text, last_name text, doc_id text, lote_id uuid) as $$
 begin
     return query
     select (prop.id, prop.creation_date, a.email, p.first_name, p.last_name, p.doc_id, l.id)
     from
    (person p join account a on p.id = a.id)
-inner join
+    inner join
     (propietario prop left join (lote l inner join lote_in_barrio lib on l.id = lib.lote_id)
         on prop.lote_id = l.id)
     on a.id = prop.user_id
