3eb9053cb4c6fda0ddb35925b057022560c389b8
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-12-27 21:15:15 -0300

    added docker

--- src/session/session.repo.ts
+++ src/session/session.repo.ts
@@ -3,14 +3,16 @@ import { Session } from './session.entity';
 export const create_session = async (
   pg,
   acc_id: string,
-  exp,
+  days_till_exp: number,
 ): Promise<Session> => {
-  return pg.query(create_session_query(acc_id, exp)).then((res) => res.rows[0]);
+  return pg
+    .query(create_session_query(acc_id, days_till_exp))
+    .then((res) => res.rows[0]);
 };
 
-const create_session_query = (acc_id: string, exp) => {
+const create_session_query = (acc_id: string, days_till_exp: number) => {
   return {
     text: 'SELECT * from insert_session($1, $2)',
-    values: [acc_id, exp],
+    values: [acc_id, days_till_exp],
   };
 };
